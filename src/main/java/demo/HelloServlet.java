64417de711139774b34ed4d080607fe96ba32ebd
mani-mr07 <144305407+mani-mr07@users.noreply.github.com>
2025-08-04 17:10:06 +0530

    inserting content

--- src/main/java/demo/HelloServlet.java
+++ src/main/java/demo/HelloServlet.java
@@ -18,6 +18,8 @@ public class HelloServlet extends HttpServlet {
 
         System.out.println("request parameter is setted");
 
+            System.out.println("param");
+
 
         RequestDispatcher dispatcher = request.getRequestDispatcher("hello.jsp");
         dispatcher.forward(request, response);
