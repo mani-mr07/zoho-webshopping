d48f1f326fca713a90d3c07b36e62f8f08459823
manikandan <emanikandan9750@gmail.com>
2025-08-05 10:15:33 +0530

    rebase

--- src/main/java/demo/HelloServlet.java
+++ src/main/java/demo/HelloServlet.java
@@ -23,6 +23,7 @@ public class HelloServlet extends HttpServlet {
 
 
 
+
         System.out.println("Dummy baba");
         System.out.println("User is not available");
         
@@ -30,6 +31,9 @@ public class HelloServlet extends HttpServlet {
         System.out.println("vanakam da maple");
 
         System.out.println("User action");
+
+        System.out.println("Dummy Baba is not used");
+
         System.out.println("User is not available");
         
 
