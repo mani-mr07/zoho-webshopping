12dc38c7bacda5aafb1121dc5c02c2a705a65c9c
mani-mr07 <144305407+mani-mr07@users.noreply.github.com>
2025-08-05 11:23:51 +0530

    Login Change

--- src/main/java/demo/LoginServlet.java
+++ src/main/java/demo/LoginServlet.java
@@ -13,6 +13,7 @@ public class LoginServlet extends HttpServlet {
 	@Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
         throws ServletException, IOException {
+		//Hi;
 
         String username = request.getParameter("username");
         String password = request.getParameter("password");
