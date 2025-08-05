ea21006643613f0e3ae8bf78baa463f2e1063de7
manikandan <emanikandan9750@gmail.com>
2025-08-05 10:01:13 +0530

    User is not available

--- src/main/java/demo/HelloServlet.java
+++ src/main/java/demo/HelloServlet.java
@@ -13,7 +13,7 @@ import jakarta.servlet.http.HttpServletResponse;
 public class HelloServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) 
         throws ServletException, IOException {
-
+    	
         request.setAttribute("name", "Manikandan");
 
         System.out.println("request parameter is setted");
@@ -22,6 +22,8 @@ public class HelloServlet extends HttpServlet {
         System.out.println("User ");
 
         System.out.println("User action");
+        System.out.println("User is not available");
+        
 
         RequestDispatcher dispatcher = request.getRequestDispatcher("hello.jsp");
         dispatcher.forward(request, response);
